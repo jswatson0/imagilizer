84152b51c9879c451c3002f5ece1ad92800c6b80
Jason Watson <jswatson0@gmail.com>
2013-07-28 14:08:14 -0500

    Add cycle method

--- custom.js
+++ custom.js
@@ -1,5 +1,28 @@
 $( document ).ready(function() {
-    alert( "ready!" );
+	
+	// check to see if file is linked correctly
+	// alert( "ready!" );
 
-    $('#panel').append($('.active'))
+	// place img with .active class in panel div 
+	// $(window).load(function(){
+	// 	$('#sidebar img').first().addClass('active').clone().appendTo('#panel');
+	// 	$('#panel img').removeAttr('width');
+	// });
+	
+	// fade out img in panel and fadeIn img from sidebar when clicked
+	// $('#sidebar img').click(function() {
+	// 	$('#panel img').first().fadeOut("slow", function() {
+	// 		$(this).remove();
+	// 	});
+	// 	$('.active').removeClass('active');
+	// 	$(this).addClass('active').clone().appendTo($('#panel')).hide();	
+	// 	$('#panel img').delay(500).fadeIn("slow");
+	// });
+
+	// cycle through imgs in #panel div
+	$('ul#slides').cycle({
+		fx: 'fade',
+		// after: function(){
+			// $('#imageCaption').html($('#slides li:visible img').data('caption'));
+	});
 });
